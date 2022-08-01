2b96aee8c271988c462dc8fa1f3c268c97b986ec
egadams <31349790+EGAdams@users.noreply.github.com>
2022-08-01 12:33:48 -0400

    component works so far.  now add led...

--- src/components/MonitoredObject.vue
+++ src/components/MonitoredObject.vue
@@ -1,27 +1,24 @@
-<!-- SourcedLogViewer component -->
+<!-- MonitoredObject component -->
 <template>
     <sourced-log-viewer 
-        data_source_type=     this.data_source_type
-        data_source_location= this.data_source_location
-        object_id=            this.object_id>
+        v-bind:data_source_type="     data_source_type     "
+        v-bind:data_source_location=" data_source_location "
+        v-bind:object_id="            object_id            ">
     </sourced-log-viewer>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import SourcedLogViewer from "@egadams/log-viewer";
-export default defineComponent( {
-    name: "SourcedLogViewer",
+import { defineComponent  } from "vue";
+import { SourcedLogViewer } from "@egadams/log-viewer";
+export default defineComponent({
+    name: "MonitoredObject",
     components: { SourcedLogViewer },
-    data () {
-        return {
-            logs: [ { id: "1", timestamp: 100, message: "ready.", method: "No Source." } ]
-        }; },
-    methods: { async clearLog () { this.logs = []; }},
+    data () {  return { this_object_id: "" }},  // to remember were data goes
+    methods: { async clearLog () { console.log( "clearing logs..." ); }},
     props: {
-        data_source_type: { type: String, default: "url" },
+        data_source_type: {     type: String, default: "url" },
         data_source_location: { type: String, default: "http://localhost:8080" },
-        object_id: { type: String, default: "MessageManager_1616" }}
+        object_id: {            type: String, default: "MessageManager_1616" }}
 } );
 </script>
 
